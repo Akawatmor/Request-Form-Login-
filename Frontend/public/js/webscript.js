399cd366661d8ad2ca18e2f1698b3461c43cd9b6
Petchon <petchsony123@gmail.com>
2024-10-14 22:13:06 +0700

    Version 1.0.3

--- Frontend/public/js/webscript.js
+++ Frontend/public/js/webscript.js
@@ -2,23 +2,73 @@ const { isUtf8 } = require('buffer');
 const { json } = require('stream/consumers');
 
 function APIRequest(){
+  const name = document.getElementById("username").value;
+  const pass = document.getElementById("password").value;
+  var json2;
 
-  fetch("https://restapi.tu.ac.th/api/v1/auth/Ad/verify", {
-    method: "POST",
-    body: JSON.stringify({
-      UserName: 6609681231,
-      PassWord: 1839901890374
-    }),
-  headers: {
-    "Content-type": "application/json; charset=UTF-8",
-    "Application-Key" : "TUecac773883f2433fc71a4432562774ce8872bf7fc11dfa548c5808ba62166ed387af71abcc56b4f6da1014ea0197c7d6"
+  if(checkField()){
 
+    fetch("https://restapi.tu.ac.th/api/v1/auth/Ad/verify", {
+      method: "POST",
+      body: JSON.stringify({
+        UserName: name,
+        PassWord: pass
+      }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+      "Application-Key" : "TUecac773883f2433fc71a4432562774ce8872bf7fc11dfa548c5808ba62166ed387af71abcc56b4f6da1014ea0197c7d6"
+    }
+
+    })
+    .then(response => response.json())
+    .then(json => {
+    console.log(json);
+    if (json.status == true){
+      console.log(json.displayname_th);
+      document.getElementById("output").innerText = "ยินดีต้อนรับ " + json.displayname_th;
+    }
+    else{
+      document.getElementById("output").innerText = "Error : " + json.message;
+    }
+    
+
+
+    })
+    .catch(error => console.error("Error : ",error));
   }
 
-  })
-  .then((response) => response.json())
-  .then((json) =>console.log(json));
+  else{
+    console.error("Error : Username or Password Cannot be blank!");
+  }
+  
 }
+
+function showPassword(){
+  var passfield = document.getElementById("password");
+  if (passfield.type == "password"){
+    passfield.type = "text";
+  }
+  else{
+    passfield.type = "password";
+  }
+}
+
+function checkField(){
+  const name = document.getElementById("username").value == "";
+  const pass = document.getElementById("password").value == "";
+  if (name+pass){
+    if(name)document.getElementById("username").style.border = "5px solid red";
+    else document.getElementById("username").style.border = "none";
+    if(pass)document.getElementById("password").style.border = "5px solid red";
+    else document.getElementById("password").style.border = "none";
+
+
+    alert("User or Password cannot be blank")
+  }
+  else
+    return 1;
+}
+
 
 
 function call_RESTAPI(){
